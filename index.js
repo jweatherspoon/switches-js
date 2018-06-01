5b6b2e1eb03f0cbf1357c05002c89ba4b41c1608
nrotroff <nrotroff@nevada.unr.edu>
2018-06-01 16:02:51 -0700

    Fixed menu

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ var indexdropdown = `<h1>What kind of switch you have?</h1>
 <select>
 <option value="FWS">FWS</option>
 <option value="ICX6450">ICX6450</option>
-<option value="ICX7250">ICX7250</option>
-<option value="ICX7150">ICX7150</option>
+<option value="ICX7250">ICX7150</option>
+<option value="ICX7150">ICX7250</option>
 <option value="ICX7450">ICX7450</option>
 </select>`
 
