7f66afd5d3c7634e38976d51709112464223bbe1
Jonathan Weatherspoon <jweatherspoon@nevada.unr.edu>
2018-06-01 16:03:35 -0700

    Moved serial out of indexjs again

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
 'use strict';
 
+const { ipcRenderer } = require('electron');
+
 const $ = require('jquery');
-
-const SerialPort = require('serialport');
-const Parsers = SerialPort.parsers;
-
-const parser = new Parsers.Readline({
-    delimeter: '\n'
-});
-
-parser.on('data', console.log);
-
-let port;
-
-/**
- * Get a list of active serial ports. Each port contains a COM name and id
- * @returns {object[]} Active serial ports
- */
-function GetPorts() {
-    var devices = [];
-    SerialPort.list((err, ports) => {
-        ports.forEach(port => {
-            if (port.pnpId) {
-                devices.push({
-                    name: port.comName,
-                    id: port.pnpId
-                });
-            }
-        });
-    });
-    return devices;
-}
-
-/**
- * Open a serial connection given a port and baudrate
- * @param {string} portname - The COM port of the serial device
- * @param {number} baudRate - The baud rate used to communicate with the device
- * @returns {SerialPort} Connection to the specified serial device
- */
-function OpenPort(portname, baudRate) {
-    return new SerialPort(portname, {
-        baudRate: baudRate
-    });
-}
 
 var indexdropdown = `<h1>What kind of switch you have?</h1>
 <select>
