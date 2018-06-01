05f5629222caeca89b66ab769526b649b60165f6
nrotroff <nrotroff@nevada.unr.edu>
2018-06-01 15:59:20 -0700

    Added dropdown

--- index.js
+++ index.js
@@ -44,8 +44,18 @@ function OpenPort(portname, baudRate) {
     });
 }
 
-$("#fader").click(function () {
+var indexdropdown = `<h1>What kind of switch you have?</h1>
+<select>
+<option value="FWS">FWS</option>
+<option value="ICX6450">ICX6450</option>
+<option value="ICX7250">ICX7250</option>
+<option value="ICX7150">ICX7150</option>
+<option value="ICX7450">ICX7450</option>
+</select>`
+
+$("#fader").click(function(){
     $("#innerdiv").fadeOut();
-    $("#innerdiv").text('Nice').next().fadein(slow);
-});
+    $("#innerdiv").text('Nice');
+    setTimeout(function() {$("#innerdiv").html(indexdropdown).fadeIn();},600)
+    });
 
