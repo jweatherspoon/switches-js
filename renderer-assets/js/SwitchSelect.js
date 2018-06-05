21311b4fe761c6a21f48cc5417a72f2564936bdf
nrotroff <nrotroff@nevada.unr.edu>
2018-06-05 15:42:49 -0700

    Added switch to forms html

--- renderer-assets/js/SwitchSelect.js
+++ renderer-assets/js/SwitchSelect.js
@@ -1,3 +1,11 @@
 if(!$) {
     const $ = require('jquery');
 }
+
+$("#switchsubmit").click(function(){
+    if ($("#switchselector").val() !== null) {
+        $(".innerdiv").fadeOut();
+        $(".innerdiv").text('Good Job');
+        setTimeout(function() {$(document.body).load('./Forms.html')},500);
+    };
+    });
