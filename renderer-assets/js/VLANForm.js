a5e54ae232015396b394e7adf5e6a03d053a8b89
nrotroff <nrotroff@nevada.unr.edu>
2018-06-07 16:25:23 -0700

    VLANForm styling

--- renderer-assets/js/VLANForm.js
+++ renderer-assets/js/VLANForm.js
@@ -1,3 +1,25 @@
 if(!$) {
     const $ = require('jquery');
 }
+
+var newvlan = `<div style="padding: 10">
+<input type="text" id="txtCustomVLAN" name="txtCustomVLAN" placeholder="Name of VLAN" style="padding-right: 10; width: 150;">
+<input class="vlaninput" type="number" id="VOIPVLAN" name="VOIPVLAN" style="margin-left: 10" placeholder="VLAN">
+</div>`
+
+customvlancount = 4;
+
+$('#btnCustomVLAN').click(function(){
+    if (customvlancount > 1) {
+        $(newvlan).insertBefore('#divbtnCustomVLAN');
+        customvlancount --;
+        $('#btnCustomVLAN').text('Add Custom VLAN (' + customvlancount + ')');
+    } else {
+        $(newvlan).insertBefore('#divbtnCustomVLAN');
+        $('#btnCustomVLAN').hide();
+    }
+})
+
+$('$btnVLANSubmit').click(function(){
+    $('#divVLANForm')
+})
