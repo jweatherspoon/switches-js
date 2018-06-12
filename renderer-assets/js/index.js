01b28c70dd9a034422d08fce4ff6b11cd85e4b0c
Jonathan Weatherspoon <jweatherspoon@nevada.unr.edu>
2018-06-12 09:45:35 -0700

    whoopy daisy

--- renderer-assets/js/index.js
+++ renderer-assets/js/index.js
@@ -7,8 +7,3 @@ $("#fader").click(function(){
     $(".innerdiv").text('Nice');
     setTimeout(function() {$(document.body).load('./SwitchSelect.html')},500);
     });
-
-ipcRenderer.send('filesys:checkver', {
-    model: "ICX 7150",
-    site: "ruckus",
-})
